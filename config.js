7764530bed4ab385a93a996e241832e13b298dca
zorancevriz <69419689+zorancevriz@users.noreply.github.com>
2021-08-02 02:34:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
   siteTitle: 'wow pvp boost for gold', // <title>
   manifestName: 'wow pvp boost for gold',
   manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: 'https://zorancevriz.github.io/',
+  manifestStartUrl: 'https://wowboostforgoldservicesmain.gatsbyjs.io/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `WoW-boost-for-gold-services`,
+ 
   heading: 'wow pvp  boost for gold ',
   subHeading: 'arena boost for gold,pvp boost for gold,mythic  boost for gold ,World of Warcraft Race to World First',
   // social
